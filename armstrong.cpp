41c64417058387e9c37f1693ea91818f5b54a5f9
palakarorraa <palakarora3958@gmail.com>
2025-09-04 22:42:46 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0, temp, digit, n = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    temp = num;
+
+    // Count digits
+    while(temp > 0) {
+        temp /= 10;
+        n++;
+    }
+
+    temp = num;
+    // Calculate sum of digits^n without pow()
+    while(temp > 0) {
+        digit = temp % 10;
+
+        int power = 1;
+        for(int i = 0; i < n; i++) {
+            power *= digit;   // digit^n
+        }
+
+        sum += power;
+        temp /= 10;
+    }
+
+    if(sum == num)
+        cout << num << " is an Armstrong number." << endl;
+    else
+        cout << num << " is not an Armstrong number." << endl;
+
+    return 0;
+}
+
