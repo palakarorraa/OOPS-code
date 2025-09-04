41defc1660738181c9ec0d04f1b936fbed64d328
palakarorraa <palakarora3958@gmail.com>
2025-09-04 09:36:28 +0530

    Add files via upload

---
+++ nested.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Calculator{
+    public:
+    int add(int a,int b)
+    {
+        return a+b;
+    }
+    void showAddition(int x,int y)
+    {
+        cout<<"Sum of "<<x<<" and "<<y<<" is: "<<add(x,y)<<endl;
+    }
+};
+int main()
+{
+    Calculator cal;
+    cal.showAddition(10,20);
+    return 0;
+}
