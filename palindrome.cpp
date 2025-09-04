d29cc41df7237d6242bd5228df831ecc4ffa399f
palakarorraa <palakarora3958@gmail.com>
2025-09-04 23:06:01 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num, rev = 0, temp, digit;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    temp = num;
+    while(temp > 0) {
+        digit = temp % 10;
+        rev = rev * 10 + digit; // reverse bna rhe h
+        temp /= 10;
+    }
+
+    if(rev == num)
+        cout << num << " is a Palindrome number." << endl;
+    else
+        cout << num << " is not a Palindrome number." << endl;
+
+    return 0;
+}
